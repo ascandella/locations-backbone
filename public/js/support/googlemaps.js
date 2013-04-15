878e7f90ddf10860916a82d6088b8b5e2d3057e4
Aiden Scandella <aiden@causes.com>
2013-04-14 21:21:33 -0700

    add empty google shim (deployment testing)

---
+++ public/js/support/googlemaps.js
@@ -0,0 +1,7 @@
+define('googlemaps', [
+  // 'async!http://maps.googleapis.com/maps/api/js?key=AIzaSyAwV2xZPg9Jj50ne5y5bjVwTnBhIIxkMkM&sensor=false'
+], function() {
+  return {
+    // maps: google.maps
+  };
+});
